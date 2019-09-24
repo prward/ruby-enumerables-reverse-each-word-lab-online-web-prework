5b33dcabb8867f9d9ed52f4188f5ed6cb05eec8c
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-24 15:45:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,5 @@ def reverse_each_word(:sentence1, :sentence2)
   Array.new = sentence1
   Array.new = sentence2
   :sentence1.split.reverse.join(' ')
-  :sentence2
+  :sentence2.split.reverse.join(' ')  
+end
