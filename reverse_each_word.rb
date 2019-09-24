387e6918a93b8306a4b28c9822933d06c06a51e8
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-24 15:56:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(string)
+  Array.new 
+  loop = string.length
+  word = ''
+  while loop > 0 
+  loop -= 1
+  word += string[loop]
+end
+end
+
+def sentence1
+  puts "Hello there, and how are you?"
+end
+
+def sentence2
+  puts "Hi again, just making sure it's reversed!"
+end
+
+ 
